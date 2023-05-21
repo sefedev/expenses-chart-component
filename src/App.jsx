d488f0d3304f06d5a636639cbdf81bf6dfd78295
sefedev <iomosefe2016@gmail.com>
2023-05-21 14:39:54 +0100

    final commit edit Readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { BalanceCard, ChartComponent } from "./components";
+import data from './data/data.json'
 
 const App = () => {
+
+  //SUM OF DATA AMOUNT
+  // const sumData = data.map(item => {
+  //   return item.amount
+  // }).reduce((prev, current) => {
+  //   return prev + current
+  // })
+
+  
   return (
     <div className="text-center bg-Cream min-h-screen grid place-items-center px-4">
       <div className="w-full md:w-1/2 lg:w-1/3">
         <BalanceCard />
-        <ChartComponent />
+        <ChartComponent data={data}/>
       </div>
     </div>
   );
--- src/components/ChartComponent/ChartComponent.jsx
+++ src/components/ChartComponent/ChartComponent.jsx
@@ -1,52 +1,69 @@
-import { Button } from 'antd'
-import React, { useState } from 'react'
-import { Bar } from 'react-chartjs-2';
-import { Chart as ChartJS} from "chart.js/auto"
 
-const data = [
-  { year: '1991', value: 3 },
-  { year: '1992', value: 4 },
-  { year: '1993', value: 3.5 },
-  { year: '1994', value: 5 },
-  { year: '1995', value: 4.9 },
-  { year: '1996', value: 6 },
-  { year: '1997', value: 7 },
-  { year: '1998', value: 9 },
-  { year: '1999', value: 13 },
-];
+import { Bar } from "react-chartjs-2";
+// eslint-disable-next-line
+import { Chart as ChartJS } from "chart.js/auto";
 
-
-const ChartComponent = () => {
+// eslint-disable-next-line
+const ChartComponent = ({ data }) => {
+  // eslint-disable-next-line
+  const maxAmount = Math.max(...data.map((item) => item.amount));
   const userData = {
-    labels: data.map(item => item.year),
-    datasets: [{
-      label: "Years Data",
-      data: data.map(item => item.value),
-      backgroundColor: ["hsl(10, 79%, 65%)"]
-    }],
-  }
+    // eslint-disable-next-line
+    labels: data.map((item) => item.day),
+    datasets: [
+      {
+        label: null,
+        // eslint-disable-next-line
+        data: data.map((item) => item.amount),
+        borderRadius: "20px",
+        // eslint-disable-next-line
+        backgroundColor: data.map((item) =>
+          item.amount === maxAmount ? "hsl(186, 34%, 60%)" : "hsl(10, 79%, 65%)"
+        ),
+        // eslint-disable-next-line
+        hoverBackgroundColor: data.map((item) =>
+        item.amount === maxAmount ? "hsl(186, 34%, 80%)" : "hsl(10, 79%, 85%)")
+      },
+    ],
+  };
 
-  
+  const option = {
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        display: false,
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
 
   return (
-    <div className='text-gray-800 bg-PaleOrange rounded-xl shadow-md px-4 py-6'>
-        <h2 className='text-2xl font-semibold'>Spending - Last 7 days</h2>
-        <div className='w-full grid place-items-center'>
-      <Bar data={userData}/>
+    <div className="text-DarkBrown bg-PaleOrange rounded-xl shadow-md px-4 py-6">
+      <h2 className="text-2xl font-semibold">Spending - Last 7 days</h2>
+      <div className="w-full grid place-items-center">
+        <Bar data={userData} options={option} />
       </div>
-      <hr className='my-6 shadow-sm'/>
-      <div className='flex justify-between'>
+      <hr className="my-6 shadow-sm" />
+      <div className="flex justify-between">
         <div>
-          <h3 className='text-gray-400'>Total this month</h3>
-          <h1 className='font-semibold text-3xl'>$478.33</h1>
+          <h3 className="text-gray-400">Total this month</h3>
+          <h1 className="font-semibold text-3xl">$478.33</h1>
         </div>
-        <div className='flex flex-col justify-end items-end'>
-          <h6 className='text-sm font-semibold'>+2.4%</h6>
-          <h4 className='text-gray-400 text-sm'>from last month</h4>
+        <div className="flex flex-col justify-end items-end">
+          <h6 className="text-sm font-semibold">+2.4%</h6>
+          <h4 className="text-gray-400 text-sm">from last month</h4>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChartComponent
+export default ChartComponent;
